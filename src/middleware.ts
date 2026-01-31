e9dd3b8ca5ee6d8e497d63bb02ebfb2c91dc4138
MdShijanAli <shijan135@gmail.com>
2026-01-31 18:35:41 +0600

    feat: implement middleware for role-based access control and session validation

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,45 +2,33 @@ import { NextRequest, NextResponse } from "next/server";
 import { userService } from "./services/user.service";
 import { Roles } from "./constants/roles";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  let isAuthenticated = false;
-  let isAdmin = false;
-  let isTutor = false;
-  let isStudent = false;
-  console.log("Proxy middleware triggered for:", request.url);
+
   const { data } = await userService.getSession();
+
   if (!data || !data.session) {
-    console.log("No active session found. Redirecting to login.");
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-  if (data.user) {
-    isAuthenticated = true;
-    isAdmin = data.user.role === Roles.ADMIN;
-    isTutor = data.user.role === Roles.TUTOR;
-    isStudent = data.user.role === Roles.STUDENT;
-  }
-  if (!isAuthenticated) {
-    console.log("User is not authenticated. Redirecting to login.");
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
+  const role = data.user?.role;
+
   if (
-    isAdmin &&
+    role === Roles.ADMIN &&
     (pathname.startsWith("/tutor") || pathname.startsWith("/student"))
   ) {
     return NextResponse.redirect(new URL("/admin/dashboard", request.url));
   }
+
   if (
-    isTutor &&
+    role === Roles.TUTOR &&
     (pathname.startsWith("/admin") || pathname.startsWith("/student"))
   ) {
     return NextResponse.redirect(new URL("/tutor/dashboard", request.url));
   }
 
   if (
-    !isAdmin &&
-    !isTutor &&
+    role === Roles.STUDENT &&
     (pathname.startsWith("/admin") || pathname.startsWith("/tutor"))
   ) {
     return NextResponse.redirect(new URL("/student/dashboard", request.url));
@@ -50,9 +38,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/admin/dashboard/:path*",
-    "/tutor/dashboard/:path*",
-  ],
+  matcher: ["/admin/:path*", "/tutor/:path*", "/student/:path*"],
 };
